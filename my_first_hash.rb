a36f5793b6605ad33f7f3b65c7f2744eb316e41f
Brendan O'Neill <oneill.brendan88@gmail.com>
2018-11-25 15:09:20 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,11 +14,9 @@ def shipping_manifest
 
 the_manifest = {
   
-  "whale bone corsets" => 5
-  "porcelain vases" => 2 
-  "oil paintings" => 3
-  
-
+  "whale_bone_corsets" => 5,
+  "porcelain_vases" => 2, 
+  "oil_paintings" => 3
 }
 
 
